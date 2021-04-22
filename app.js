bfa1f85d5eb5fe9f055be79e594a018ee6c77829
MOMOJMOGG <momomacaca@gmail.com>
2021-04-22 22:17:37 +0800

    feat: display show and dynamic router

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // pass the restaurant data into 'index' partial template
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// dynamic router using params
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(rest => rest.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
 })
 
 app.listen(port, () => {
