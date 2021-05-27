dddfd49b96b9db8c8d9515062deba30b1ee9cfac
MOMOJMOGG <momomacaca@gmail.com>
2021-05-27 22:08:44 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const hbshelpers = require("handlebars-helpers")
 const methodOverride = require('method-override')
+const session = require('express-session')
 const routes = require('./routes') // 引用路由器
 require('./config/mongoose') // 引用資料庫
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const multihelpers = hbshelpers()
 const app = express()
-const port = 3000
+const PORT = process.env.PORT
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
@@ -23,11 +28,17 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
 // 將 request 導入路由器
 app.use(routes)
 
 
 // listening
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}.`)
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}.`)
 })
