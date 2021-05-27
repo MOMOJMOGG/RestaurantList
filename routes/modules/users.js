49bbacdcae20e605dd9213f2334fa3d6ff5420ef
MOMOJMOGG <momomacaca@gmail.com>
2021-05-27 22:03:03 +0800

    feat: add register process

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -15,7 +17,47 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  console.log(req.body)
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: '有必填欄位都是空白。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    console.log(errors)
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
 
+  User.findOne({ email }).then(user => {
+    // 如果已經註冊：退回原本畫面
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
 })
 
 router.get('/logout', (req, res) => {
