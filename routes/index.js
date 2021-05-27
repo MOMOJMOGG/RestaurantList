8f1e4c23f04fc32fb3a9b33b636a343d4f957c6b
MOMOJMOGG <momomacaca@gmail.com>
2021-05-27 21:17:32 +0800

    feat: init user router & login, register page

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const router = express.Router()
 // 準備引入路由模組
 // 引入 home 模組程式碼
 const home = require('./modules/home')
-// 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/', home)
-
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
+
+
 router.use('/restaurants', restaurants)
+router.use('/users', users)
+router.use('/', home)
 
 // 匯出路由器
 module.exports = router
